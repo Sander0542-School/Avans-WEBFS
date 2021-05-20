38ded4c9bbd98d3b029506b1de74b5cca5d43b36
Sander Jochems <sanderjochems@hotmail.nl>
2021-05-20 16:43:02 +0200

    Create basic tables

---
+++ database/migrations/2021_05_20_133448_create_dishes_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained('menu_categories', 'id');
+            $table->string('number', 4);
+            $table->string('addition', 4)->nullable();
+            $table->string('name');
+            $table->string('description');
+            $table->decimal('price',5,2);
+            $table->tinyInteger('btw')->default(9);
+            $table->tinyInteger('spiciness_level')->nullable();
+
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->unique(['number', 'addition']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dishes');
+    }
+}
---
+++ database/migrations/2021_05_20_142447_create_order_dishes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderDishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_dishes', function (Blueprint $table)
+        {
+            $table->id();
+            $table->foreignId('dish_id')->constrained('dished', 'id');
+            $table->foreignId('order_id')->constrained('orders', 'id');
+            $table->smallInteger('amount')->default(1);
+            $table->decimal('unit_price', 5, 2);
+            $table->tinyInteger('btw')->default(9);
+            $table->string('remark')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_dishes');
+    }
+}
