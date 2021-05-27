b9e27e71efbad262239ac793540c3e9e83db1eb9
Sander Jochems <sanderjochems@hotmail.nl>
2021-05-27 16:49:33 +0200

    Fix vue home components

--- resources/js/Pages/Home/Menu.vue
+++ resources/js/Pages/Home/Menu.vue
@@ -1,35 +1,35 @@
 <template>
-  <home-layout>
-    <div class="card-columns">
-      <div v-for="category in menu" class="menu">
-        <div v-if="category.dishes.length > 0">
-          <h5>{{ category.name }}</h5>
-          <div v-for="dish in category.dishes">
-            <div class="menu-item">
-              <span>{{ dish.number }}</span>
-              <span v-if="dish.addition">{{ dish.addition }}</span>
-              <span>. {{ dish.name }}</span>
-              <span class="dotted"></span>
-              <span>&euro; {{ dish.price }}</span>
+    <home-layout>
+        <div class="card-columns">
+            <div v-for="category in menu" class="menu">
+                <div v-if="category.dishes.length > 0">
+                    <h5>{{ category.name }}</h5>
+                    <div v-for="dish in category.dishes">
+                        <div class="menu-item">
+                            <span>{{ dish.number }}</span>
+                            <span v-if="dish.addition">{{ dish.addition }}</span>
+                            <span>. {{ dish.name }}</span>
+                            <span class="dotted"></span>
+                            <span>&euro; {{ dish.price }}</span>
+                        </div>
+                        <span v-if="dish.description" class="font-italic small">({{ dish.description }})</span>
+                    </div>
+                    <br/>
+                </div>
             </div>
-            <span v-if="dish.description" class="font-italic small">({{ dish.description }})</span>
-          </div>
-          <br/>
         </div>
-      </div>
-    </div>
-  </home-layout>
+    </home-layout>
 </template>
 
 <script>
 import HomeLayout from '@/Layouts/HomeLayout';
 
 export default {
-  components: {
-    HomeLayout
-  },
-  props: {
-    menu: Array
-  }
+    components: {
+        HomeLayout
+    },
+    props: {
+        menu: Array
+    }
 }
 </script>
