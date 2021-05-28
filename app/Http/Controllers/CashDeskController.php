0d5b857dc0bfac1aabfef3b04a7cf1334fefa065
Tommy den Reijer <t.ommy@hotmail.nl>
2021-05-28 14:57:17 +0200

    created controller

---
+++ app/Http/Controllers/CashDeskController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MenuCategory;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class CashDeskController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('CashDesk/Index');
+    }
+
+    public function dishes()
+    {
+        return Inertia::render('CashDesk/Dishes', [
+            'menu' => MenuCategory::with('dishes')->get(),
+        ]);
+    }
+
+    public function contact()
+    {
+        return Inertia::render('CashDesk/Cart');
+    }
+}
---
+++ resources/js/Pages/CashDesk/Dishes.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "Dishes"
+}
+</script>
+
+<style scoped>
+
+</style>
