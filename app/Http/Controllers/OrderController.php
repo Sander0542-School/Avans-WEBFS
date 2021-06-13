dca5e97cdb724c95a2c59eceb5fa87418e1b2ba2
Sander Jochems <sanderjochems@hotmail.nl>
2021-06-13 15:21:40 +0200

    Fix MenuCategory Model

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Allergy;
 use App\Models\Dish;
 use App\Models\MenuCategory;
 use Inertia\Inertia;
@@ -12,13 +11,8 @@ class OrderController extends Controller
     public function index()
     {
         return Inertia::render('Order/Index', [
-            'menu' => MenuCategory::all()->map(function (MenuCategory $category) {
-                return [
-                    'id' => $category->id,
-                    'name' => $category->name,
-                    'extra_option' => $category->extra_option,
-                ];
-            }),
+            'menu' => MenuCategory::menuData(false),
+            'dishes' => Dish::cartData(),
         ]);
     }
 
@@ -29,23 +23,7 @@ class OrderController extends Controller
                 'id' => $category->id,
                 'name' => $category->name,
                 'extra_option' => $category->extra_option,
-                'dishes' => $category->dishes->map(function (Dish $dish) {
-                    return [
-                        'id' => $dish->id,
-                        'number' => $dish->number,
-                        'addition' => $dish->addition,
-                        'name' => $dish->name,
-                        'description' => $dish->description,
-                        'price' => $dish->price_inc,
-                        'spiciness' => $dish->spiciness_level ?? 0,
-                        'allergies' => $dish->allergies->map(function (Allergy $allergy) {
-                            return [
-                                'name' => $allergy->name,
-                                'icon' => $allergy->icon,
-                            ];
-                        }),
-                    ];
-                }),
+                'dishes' => $category->menuDishes(),
             ],
         ]);
     }
